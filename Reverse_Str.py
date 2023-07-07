1dc498fc7fc18247dff236eb70e64e4a2541d8b6
RaJ-0002 <122745727+RaJ-0002@users.noreply.github.com>
2023-07-07 23:15:38 +0530

    Update Reverse_Str.py
    Code modified with loop and input function.

--- Reverse_Str.py
+++ Reverse_Str.py
@@ -1,3 +1,15 @@
-n=input()
-length = len(n)
-print(n[::-1],end="")
+#Write a Python program that accepts a word from the user and reverse it.
+
+#Sample Test Case
+#Input : Edyoda
+#output: adoydE
+
+
+word = input("Input:")
+reversed_word = ''
+
+index = len(word) - 1
+while index >= 0:
+    reversed_word += word[index]
+    index -= 1
+print("Output:", reversed_word)
